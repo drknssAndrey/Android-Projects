c76d00a02bba05e6567e32f0c08c6629f24c8274
Andrey Balbino <andreymelo98@gmail.com>
2019-05-30 10:09:50 -0300

    Api configurada

---
+++ YoutubeApp/app/src/main/java/com/andreyprojects/youtubeapp/curso/android/MainActivity.java
@@ -0,0 +1,15 @@
+package com.andreyprojects.youtubeapp.curso.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String GOOGLE_APY_KEY = "AIzaSyAIOKD6nSj8eTOcsHGK2_aZYvkVUTcUs5k";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
